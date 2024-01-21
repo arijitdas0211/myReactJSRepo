03d225248e9aec2f0b4ea08cb7108ce91873d6f6
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-01-21 14:24:27 +0530

    update

--- src/components/Alert.js
+++
@@ -1,8 +0,0 @@
-export default function Alert(prop) {
-    return (
-        prop.alert && <div className={`alert alert-${prop.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{prop.alert.msg}</strong>
-            {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
-        </div>
-    );
-}
